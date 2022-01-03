97e169dd291b9e47975fbb0c94f71e7dc99cbf51
Sultan SARIZEYBEK <85437211+Sarizeybekk@users.noreply.github.com>
2022-01-03 18:36:43 +0300

    Add files via upload

---
+++ TMSearching/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtKaynak = new System.Windows.Forms.TextBox();
+            this.txtAranan = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(261, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Aşama";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(412, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Durum";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(710, 396);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sonuç";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(15, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "String Giriniz";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Location = new System.Drawing.Point(12, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Aranacak string";
+            // 
+            // txtKaynak
+            // 
+            this.txtKaynak.Location = new System.Drawing.Point(125, 70);
+            this.txtKaynak.Name = "txtKaynak";
+            this.txtKaynak.Size = new System.Drawing.Size(319, 22);
+            this.txtKaynak.TabIndex = 6;
+            // 
+            // txtAranan
+            // 
+            this.txtAranan.Location = new System.Drawing.Point(125, 109);
+            this.txtAranan.Name = "txtAranan";
+            this.txtAranan.Size = new System.Drawing.Size(319, 22);
+            this.txtAranan.TabIndex = 7;
+            this.txtAranan.TextChanged += new System.EventHandler(this.txtAranan_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.button1.ImageIndex = 7;
+            this.button1.ImageList = this.ımageList1;
+            this.button1.Location = new System.Drawing.Point(18, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 69);
+            this.button1.TabIndex = 8;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.button2.ImageIndex = 4;
+            this.button2.ImageList = this.ımageList1;
+            this.button2.Location = new System.Drawing.Point(158, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 69);
+            this.button2.TabIndex = 9;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Location = new System.Drawing.Point(331, 150);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Location = new System.Drawing.Point(480, 150);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 17);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Location = new System.Drawing.Point(621, 150);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 17);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Location = new System.Drawing.Point(781, 396);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(54, 17);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "label10";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.ImageIndex = 6;
+            this.button3.ImageList = this.ımageList1;
+            this.button3.Location = new System.Drawing.Point(604, 63);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 68);
+            this.button3.TabIndex = 14;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "search (1).png");
+            this.ımageList1.Images.SetKeyName(1, "exit.png");
+            this.ımageList1.Images.SetKeyName(2, "reset.png");
+            this.ımageList1.Images.SetKeyName(3, "search.png");
+            this.ımageList1.Images.SetKeyName(4, "undo-arrow.png");
+            this.ımageList1.Images.SetKeyName(5, "x-button.png");
+            this.ımageList1.Images.SetKeyName(6, "button.png");
+            this.ımageList1.Images.SetKeyName(7, "search (2).png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(889, 449);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtAranan);
+            this.Controls.Add(this.txtKaynak);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtKaynak;
+        private System.Windows.Forms.TextBox txtAranan;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ImageList ımageList1;
+    }
+}
+
---
+++ TMSearching/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label7.Text = string.Empty;
+            label8.Text = string.Empty;
+            label9.Text = string.Empty;
+            label10.Text = string.Empty;
+        }
+        void bulYazdir(int k,char[] f)
+        {
+            label10.Text = string.Empty;
+            for (int a = 0; a < k; a++)
+                label10.Text = label10.Text + f[a];
+                label10.Text = label10.Text + " Bulundu ";
+        }
+        List<TextBox> serit1 = new List<TextBox>();
+        List<TextBox> serit2 = new List<TextBox>();
+
+        private void button1_Click(object sender, EventArgs e)//çalıştır butonumuz
+        {
+            int blnKSay = 0;//bulunan karaktersayısı
+            string bos = "0";
+            string Kaynak = txtKaynak.Text.ToString();//gelen veriler degiişkene atanır.
+            string Aranan = txtAranan.Text.ToString();//
+            Kaynak = Kaynak.Insert(0, bos);
+            Kaynak = Kaynak.Insert(Kaynak.Length, bos);
+            char[] serit = Kaynak.ToCharArray();//gelenler char array dizisine aktarılır.
+            char[] ara = Aranan.ToCharArray();
+            for (int j=0; j <Kaynak.Length; j++)
+            {
+                var kutu1 = new TextBox();
+                kutu1.Size = new Size(20, 20);
+                kutu1.Location = new Point(10 + j * 25, 190);
+                kutu1.Name = "Username" + j;
+                kutu1.Tag = "Dinamik";
+                kutu1.Text = serit[j].ToString();
+                Controls.Add(kutu1);
+                serit1.Add(kutu1);
+            }
+            for (int l = 0; l < Aranan.Length; l++)
+            {
+                var kutu2 = new TextBox();
+                kutu2.Size = new Size(20, 20);
+                kutu2.Location = new Point(10 + l * 25, 110);
+                kutu2.Name = "Username" + l;
+                kutu2.Tag = "Dinamik";
+                kutu2.Text = ara[l].ToString();
+                Controls.Add(kutu2);
+                serit2.Add(kutu2);
+            }
+            int y = 0;
+            for (int i = 0; i < Aranan.Length; i++)
+            {
+                label7.Text = i + " Aşama " + Aranan[i] + "aranıyor...";
+                while (y<Kaynak.Length)
+                {
+                    serit1[y].BackColor = Color.DeepPink;
+                    label8.Text = "arama başladı" + y + "Karaktere bakılıyor";
+                    if (serit[y]==ara[i])
+                    {
+                        blnKSay++;
+                        label10.Text = "";
+                        bulYazdir(blnKSay, ara);
+                        label8.Text = "q" + i;
+                        if (blnKSay==Aranan.Length)
+                        {
+                            serit1[y].BackColor = Color.Red;
+                            serit2[i].BackColor = Color.Red;
+                            label8.Text = "arama tamamlandı string bulundu";
+                            label7.Text = "arama tamamlandı";
+                            MessageBox.Show("kelime bulundu");
+                            i = Aranan.Length;
+                            y = Kaynak.Length;
+                            break;
+                        }
+                        else
+                        {
+                            serit1[y].BackColor = Color.Red;
+                            serit2[i].BackColor = Color.Red;
+                            MessageBox.Show(ara[i] + "karakter bulundu");
+                            label7.Text = blnKSay + "bulundu bir sonraki karaktere geçilir";
+                            label10.Text = "";
+                            bulYazdir(blnKSay, ara);
+                            y++;
+                            break;
+                        }
+                    }
+                    else if (serit[y].ToString()==bos && y>0)
+                    {
+                        label8.Text = "bulunamadı pozisyon";
+                        label7.Text = "arama taamlandı";
+                        i = Aranan.Length;
+                        y = Kaynak.Length;
+                        break;
+                    }
+                    else
+                    {
+                        label8.Text = ara[i] + "bulunamadı";
+                        y++;
+                        i = -1;
+                        label9.Text = "";
+                        label10.Text = "";
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)//exit butonu
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)//reset butonu
+        {
+            foreach (var textBox in serit1)
+            {
+                if (textBox.Tag.ToString() == "Dinamik")
+                    textBox.Dispose();
+            }
+            serit2.Clear();
+            txtKaynak.Text = string.Empty;
+            txtAranan.Text = string.Empty;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAranan_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
